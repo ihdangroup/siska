b31c4e6ca429665f3dfd64fe9f60efeb950c0e88
Ikhdan <maghikhdan@gmail.com>
2024-05-15 18:20:49 +0700

    Membuat controller dan view dengan bantuan Gii

---
+++ frontend/controllers/SuplierController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace frontend\controllers;
+
+class SuplierController extends \yii\web\Controller
+{
+    public function actionView()
+    {
+        return $this->render('view');
+    }
+
+}
