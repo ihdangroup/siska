2edb9076adf4ce38fa30153329979fbf1f609bf5
Ikhdan <maghikhdan@gmail.com>
2024-08-09 14:16:58 +0700

    Tweak tampilan untuk model Universitas

--- siska/views/universitas/index.php
+++ siska/views/universitas/index.php
@@ -3,8 +3,8 @@
 use siska\models\Universitas;
 use yii\helpers\Html;
 use yii\helpers\Url;
-use yii\grid\ActionColumn;
-use yii\grid\GridView;
+use kartik\grid\GridView;
+use kartik\grid\ActionColumn;
 use yii\widgets\Pjax;
 /** @var yii\web\View $this */
 /** @var siska\models\UniversitasSearch $searchModel */
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Universitas', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Tambah Universitas', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php Pjax::begin(); ?>
--- siska/views/universitas/update.php
+++ siska/views/universitas/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var siska\models\Universitas $model */
 
-$this->title = 'Update Universitas: ' . $model->id;
+$this->title = 'Ubah Universitas: ' . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Universitas', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Ubah';
 ?>
 <div class="universitas-update">
 
